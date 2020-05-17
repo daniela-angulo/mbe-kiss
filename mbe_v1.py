141e0453e9b8f05a2ff7ac4db96dcbaaa9857ef4
dani <dangulom@unal.edu.co>
2020-05-17 17:44:43 -0400

    just starting here, nothing to report

---
+++ mbe_v1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib as mpl 
+from matplotlib import pylab as plt
+from numpy import linalg
+from scipy.special import factorial
+import time as time
+from scipy import special
+
+mpl.rcParams.update({'font.size': 14,'font.family': 'STIXGeneral',
+                            'mathtext.fontset': 'cm'})
